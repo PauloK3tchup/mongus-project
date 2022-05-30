879ea84c8534b27c21df2126011cd5cfab2c3e64
paulozemr <guilherme.gui.tama@gmail.com>
2022-05-30 15:15:02 -0300

    Bolas

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <header>
+    Cabeçalho
+  </header>
+  <main>
+    <h1>Among us jumpscare (very scary)</h1>
+  </main>
+  <footer>
+    Rodapau
+  </footer>
+</template>
+
+<style>
+@import '@/assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  /* padding: 2rem; */
+  height: 100vh;
+
+  font-weight: normal;
+}
+
+h1{
+  transition: 1.25s;
+  width: fit-content;
+  color: rgb(0, 0, 0);
+  cursor: pointer;
+}
+
+h1:hover{
+  transform: matrix(1000, 70, 60, 50, 40, 30);
+  color: black;
+}
+
+header, footer{
+  background-color: red;
+  color: white;
+  height: 15%;
+  font-size: 1.3rem;
+  display: flex;
+  align-items: center;
+  padding-left: 2rem;
+}
+
+</style>
